51144e62db39b92c0d928c4fd7a41e088578b719
Carlos Wilker <cwilkersantana@gmail.com>
2022-08-25 20:37:13 -0300

    adicao de conteudo de pratica para djangorest

---
+++ curso-djangorest-framework/project/settings/installed_apps.py
@@ -0,0 +1,15 @@
+INSTALLED_APPS = [
+    # django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # django rest framework
+    'rest_framework',
+    # Estes são nossos apps
+    'recipes',
+    'authors',
+    'tag',
+]
