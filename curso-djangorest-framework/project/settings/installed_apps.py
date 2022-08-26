3c24b9b5ddbeedfc24ecb72fd086e242ed0130e9
Carlos Wilker <cwilkersantana@gmail.com>
2022-08-26 12:15:00 -0300

    updates nos arquivos

--- curso-djangorest-framework/project/settings/installed_apps.py
+++ curso-djangorest-framework/project/settings/installed_apps.py
@@ -1,12 +1,12 @@
 INSTALLED_APPS = [
-    # django
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # django rest framework
+    # Django rest framework
     'rest_framework',
     # Estes são nossos apps
     'recipes',
--- curso-djangorest-framework/recipes/signals.py
+++ curso-djangorest-framework/recipes/signals.py
@@ -16,6 +16,7 @@ def delete_cover(instance):
 @receiver(pre_delete, sender=Recipe)
 def recipe_cover_delete(sender, instance, *args, **kwargs):
     old_instance = Recipe.objects.filter(pk=instance.pk).first()
+
     if old_instance:
         delete_cover(old_instance)
 
@@ -23,10 +24,10 @@ def recipe_cover_delete(sender, instance, *args, **kwargs):
 @receiver(pre_save, sender=Recipe)
 def recipe_cover_update(sender, instance, *args, **kwargs):
     old_instance = Recipe.objects.filter(pk=instance.pk).first()
-    
+
     if not old_instance:
         return
-    
+
     is_new_cover = old_instance.cover != instance.cover
 
     if is_new_cover:
